fd837789ad01ff8f8f82572eafc87a9f8839daa2
uma chandra <umachandra4821@gmail.com>
2026-04-22 11:53:09 +0530

    Inital commit

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#typed", {
+  strings: ["Web Developer"],
+  typeSpeed: 80,
+  backSpeed: 40,
+  loop: true
+});
+function toggleMenu() {
+  document.getElementById("sideMenu").classList.toggle("active");
+}
